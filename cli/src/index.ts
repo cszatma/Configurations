4601e97a2a3454cce1c674b93a368a7a124653ca
cszatma <christopherszatmary@gmail.com>
2018-04-20 23:56:18 -0400

    Imported type-extensions, added more options to add command, added check that at least one argument was passed to the program otherwise the help is displayed

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -6,6 +6,7 @@ import fs from 'fs-extra';
 
 import addCommand from './commands/add';
 import stripScope from './utils/strip-scope';
+import './utils/type-extensions';
 
 const packageJson = require('../package.json');
 
@@ -27,7 +28,18 @@ program
         '-p, --path <path>',
         'Path to add config file, defaults to current directory',
     )
+    .option('-n, --name <name>', 'Custom file name for the config file')
+    .option(
+        '-t, --type <type>',
+        'File type, either js or json, defaults to json',
+    )
+    .option('-a, --append', 'Appends the config file to your package.json')
     .option('-f, --force', 'Overwrites a config file if it already exists')
     .action(addCommand);
 
 program.parse(process.argv);
+
+if (program.args.length < 1) {
+    program.help();
+    process.exit(0);
+}
