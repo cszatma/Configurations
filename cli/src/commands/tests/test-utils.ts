1947d642c43c3258fd8865c4007b6c0a972a9814
cszatma <christopherszatmary@gmail.com>
2018-04-25 16:49:58 -0400

    Added test-utils.ts and moved runConfigGenCLI() into there so it can be used by all test files

---
+++ cli/src/commands/tests/test-utils.ts
@@ -0,0 +1,40 @@
+import { spawn } from 'child_process';
+import 'jest';
+
+export interface CLIStatus {
+    code: number;
+    stdout: string;
+    stderr?: string;
+}
+
+export const CONFIG_GEN_PATH = require.resolve('../../config-gen');
+export const TS_NODE = require.resolve('ts-node/dist/bin');
+
+export function runConfigGenCLI(args: string): Promise<CLIStatus> {
+    return new Promise<CLIStatus>((resolve, reject) => {
+        let stdout = '';
+        let stderr = '';
+
+        const childProcess = spawn(TS_NODE, [CONFIG_GEN_PATH, args], {
+            cwd: __dirname,
+            shell: true,
+        });
+
+        childProcess.on('error', error => reject(error));
+        childProcess.stdout.on('data', data => {
+            stdout += data.toString();
+        });
+
+        childProcess.stderr.on('data', data => {
+            stderr += data.toString();
+        });
+
+        childProcess.on('close', code => {
+            if (stderr) {
+                reject({ code, stderr, stdout });
+            } else {
+                resolve({ code, stdout });
+            }
+        });
+    });
+}
