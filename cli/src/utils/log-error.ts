b9776f14a9cf2529e26fee628c395295e129aa64
cszatma <christopherszatmary@gmail.com>
2018-04-24 23:55:14 -0400

    Renamed log-error.ts to process-utils.ts, added logSuccess and various other useful functions, added tests for them

--- cli/src/utils/log-error.ts
+++
@@ -1,5 +0,0 @@
-import chalk from 'chalk';
-
-export default function logError(message?: any, ...optionalParams: any[]) {
-    console.error(chalk.red(message, ...optionalParams));
-}
---
+++ cli/src/utils/process-utils.ts
@@ -0,0 +1,31 @@
+import chalk from 'chalk';
+import path from 'path';
+
+export function logError(message?: any, ...optionalParams: any[]) {
+    console.error(chalk.red(message, ...optionalParams));
+}
+
+export function logSuccess(message?: any, ...optionalParams: any[]) {
+    console.log(chalk.green(message, ...optionalParams));
+}
+
+export function exitFailure(
+    message?: any,
+    statusCode: number = 1,
+    ...optionalParams: any[]
+) {
+    logError(message, ...optionalParams);
+    process.exit(statusCode);
+}
+
+export function exitSuccess(message?: any, ...optionalParams: any[]) {
+    logSuccess(message, ...optionalParams);
+    process.exit(0);
+}
+
+export function currentDir(): string {
+    return process
+        .cwd()
+        .split(path.sep)
+        .last();
+}
