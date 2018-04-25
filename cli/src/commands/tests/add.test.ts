b6bfb92428a4faf64ca59b17a842d79105b11174
cszatma <christopherszatmary@gmail.com>
2018-04-25 16:51:09 -0400

    Rewrote add tests to resolve issues with the way the tests were done

--- cli/src/commands/tests/add.test.ts
+++ cli/src/commands/tests/add.test.ts
@@ -1,11 +1,8 @@
-import { spawn } from 'child_process';
 import fs from 'fs-extra';
 import 'jest';
 import path from 'path';
-import configTypes from '../../utils/config-types';
 
-const CONFIG_GEN_PATH = require.resolve('../../config-gen');
-const TS_NODE = require.resolve('ts-node/dist/bin');
+import { runConfigGenCLI } from './test-utils';
 
 const resolveFile = (file: string) => path.resolve(__dirname, file);
 
@@ -15,14 +12,8 @@ describe('Tests for the add command', () => {
     });
 
     afterAll(() => {
-        ['.prettierrc.json', '.eslintrc.js', 'package.json'].forEach(file =>
-            fs.unlinkSync(resolveFile(file)),
-        );
-    });
-
-    it('should print the help for config-gen', () => {
-        return runConfigGenCLI('--help').then(({ stdout }) => {
-            expect(stdout).toMatchSnapshot();
+        ['.prettierrc.json', '.eslintrc.js', 'package.json'].forEach(file => {
+            fs.unlinkSync(resolveFile(file));
         });
     });
 
@@ -32,89 +23,52 @@ describe('Tests for the add command', () => {
         });
     });
 
-    it('should create a .prettierrc.json file', () => {
-        return runCLIAndAssertFileOutput('add prettier', '.prettierrc.json');
+    it('should create a .prettierrc.json file', async () => {
+        const { code, stdout } = await runConfigGenCLI('add prettier');
+        const file = resolveFile('.prettierrc.json');
+
+        expect(code).toBe(0);
+        expect(stdout).toBe(`Successfully wrote prettier config to ${file}.\n`);
+        expect(fs.existsSync(file)).toBe(true);
     });
 
-    it('should create a .eslintrc.js file', () => {
-        return runCLIAndAssertFileOutput(
-            'add .eslintrc --type js',
-            '.eslintrc.js',
-        );
+    it('should create a .eslintrc.js file', async () => {
+        const { code, stdout } = await runConfigGenCLI('add eslint --type js');
+        const file = resolveFile('.eslintrc.js');
+
+        expect(code).toBe(0);
+        expect(stdout).toBe(`Successfully wrote eslint config to ${file}.\n`);
+        expect(fs.existsSync(file)).toBe(true);
     });
 
     it('should return an error since eslintrc.js already exists', () => {
-        return runCLIAndAssertError('add eslint --type js');
-    });
-
-    it('should create the file even though it exists since -w is passed', () => {
-        return runCLIAndAssertFileOutput('add prettier -w', '.prettierrc.json');
-    });
-
-    it('should add an eslintConfig object to package.json', () => {
-        return runCLIAndAssertPackageJson('add eslint -P', 'eslint');
-    });
-});
-
-interface CLIStatus {
-    stdout?: string;
-    stderr?: string;
-    code: number;
-}
-
-function runConfigGenCLI(args: string): Promise<CLIStatus> {
-    return new Promise<CLIStatus>((resolve, reject) => {
-        let stdout = '';
-        let stderr = '';
-
-        const childProcess = spawn(TS_NODE, [CONFIG_GEN_PATH, args], {
-            cwd: __dirname,
-            shell: true,
-        });
-
-        childProcess.on('error', error => reject(error));
-        childProcess.stdout.on('data', data => {
-            stdout += data.toString();
-        });
-
-        childProcess.stderr.on('data', data => {
-            stderr += data.toString();
-        });
-
-        childProcess.on('close', code => {
-            if (stderr) {
-                reject({ stderr, code });
-            } else {
-                resolve({ stdout, code });
-            }
+        return runConfigGenCLI('add prettier').catch(({ code, stderr }) => {
+            expect(code).toBe(1);
+            expect(stderr).toBe('.prettierrc.json already exists! Aborting.\n');
         });
     });
-}
 
-function runCLIAndAssertFileOutput(args: string, fileName: string) {
-    return runConfigGenCLI(args).then(({ stdout, code }) => {
-        expect(stdout).toMatchSnapshot();
+    it('should create the file even though it exists since -w is passed', async () => {
+        const { code, stdout } = await runConfigGenCLI('add prettier -w');
+        const file = resolveFile('.prettierrc.json');
+
         expect(code).toBe(0);
-        expect(fs.existsSync(resolveFile(fileName))).toBe(true);
+        expect(stdout).toBe(`Successfully wrote prettier config to ${file}.\n`);
+        expect(fs.existsSync(file)).toBe(true);
     });
-}
 
-function runCLIAndAssertPackageJson(args: string, configName: string) {
-    return runConfigGenCLI(args).then(({ stdout, code }) => {
-        expect(stdout).toMatchSnapshot();
+    it('should add an eslintConfig object to package.json', async () => {
+        const { code, stdout } = await runConfigGenCLI(
+            'add eslint --package-json',
+        );
+
         expect(code).toBe(0);
+        expect(stdout).toBe(
+            'Successfully wrote eslint config to package.json.\n',
+        );
 
         const packageJson = require(resolveFile('package.json'));
-        const template = require(resolveFile('../../templates/' + configName));
-        const configPackageJsonName =
-            configTypes[configName].fileNames.packageJson;
-        expect(packageJson[configPackageJsonName]).toEqual(template);
+        const template = require(resolveFile('../../templates/eslint'));
+        expect(packageJson.eslintConfig).toEqual(template);
     });
-}
-
-function runCLIAndAssertError(args: string) {
-    return runConfigGenCLI(args).catch(({ stderr, code }) => {
-        expect(stderr).toMatchSnapshot();
-        expect(code).toBe(1);
-    });
-}
+});
