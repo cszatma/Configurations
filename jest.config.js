24d1587a4d94938b370fd3202e73c61f28efd59c
Christopher Szatmary <cs@christopherszatmary.com>
2019-04-24 17:37:48 -0400

    Fixed jest config

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   globals: {
     'ts-jest': {
-      tsConfigFile: 'tsconfig.json',
+      tsConfig: 'tsconfig.json',
     },
   },
   moduleFileExtensions: ['ts', 'js'],
   transform: {
-    '^.+\\.(ts|tsx)$': './node_modules/ts-jest/preprocessor.js',
+    '^.+\\.tsx?$': 'ts-jest',
   },
   testMatch: ['**/tests/**/*.test.(ts|js)'],
   testEnvironment: 'node',
