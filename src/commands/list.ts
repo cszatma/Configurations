d5fcc1a1739283c7a0bd33fde0f4098701fffb7d
Christopher Szatmary <cs@christopherszatmary.com>
2019-04-28 16:18:50 -0400

    list command now lists custom configurations

--- src/commands/list.ts
+++ src/commands/list.ts
@@ -1,19 +1,34 @@
 import configTypes from '../utils/config-types';
 import { logError } from '../utils/process-utils';
+import { loadOptions } from '../utils/options';
 
 export default function list(config?: string) {
-  if (!config) {
+  if (config) {
+    listAvailableFileTypes(config);
+  } else {
     listAllCommands();
-    return process.exit(0);
   }
-
-  listAvailableFileTypes(config);
 }
 
 function listAllCommands() {
-  console.log('Available configurations:');
-  Object.values(configTypes).forEach(config =>
-    console.log(' - ' + config.name),
+  printList(
+    'Default configurations:',
+    Object.values(configTypes),
+    config => config.name,
+  );
+
+  const customConfigs = loadOptions().customConfigs;
+  const keys = Object.keys(customConfigs);
+
+  if (keys.length === 0) {
+    return;
+  }
+
+  // Print custom configs
+  printList(
+    'Custom configurations:',
+    keys,
+    key => `${key} (${customConfigs[key]})`,
   );
 }
 
@@ -26,9 +41,20 @@ function listAvailableFileTypes(configName: string) {
     process.exit(1);
   }
 
-  console.log(`Available file types for ${configName}:`);
-  config.fileTypes.forEach(fileType => console.log(' - ' + fileType));
+  printList(`Available file types for ${configName}:`, config.fileTypes);
   console.log(
     `Supports package.json: ${config.supportsPackageJson ? 'yes' : 'no'}`,
   );
+}
+
+function printList<T>(
+  title: string,
+  array: T[],
+  formatItem?: (element: T) => string,
+) {
+  console.log(title);
+  array.forEach(element => {
+    const item = formatItem ? formatItem(element) : element;
+    console.log(` - ${item}`);
+  });
 }
