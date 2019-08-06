ce95aa8e57d2fb14ca442184d7abb27866ab698d
Christopher Szatmary <cs@christopherszatmary.com>
2019-08-05 20:36:30 -0400

    Deleted collection-utils

--- src/utils/collection-utils.ts
+++
@@ -1,8 +0,0 @@
-export interface Collection<T> {
-  length: number;
-  [key: number]: T;
-}
-
-export const lastElement = <T>(collection: Collection<T>): T => {
-  return collection[collection.length - 1];
-};
