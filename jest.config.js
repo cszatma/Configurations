9161be251a3a7d38a70ed109bea81536ca0fe184
Christopher Szatmary <cs@christopherszatmary.com>
2019-08-18 15:50:06 -0400

    Ignore .d.ts files in coverage

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   collectCoverageFrom: ['src/**/*.(js|ts)'],
+  coveragePathIgnorePatterns: ['src/templates', '.d.ts'],
   coverageReporters: ['html', 'text', 'lcov'],
   modulePaths: ['<rootDir>/src', '<rootDir>'],
 };
